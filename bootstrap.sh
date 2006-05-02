84b0ca49af13ffbbb70245f9714686089af24eab
David Leonard <david.leonard@quest.com>
2006-05-02 15:04:23 +0000

    r31: more portability work

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,5 +2,10 @@
 # $Id$
 # 
 
-rm -rf autom4te.cache install-sh missing Makefile.in configure aclocal.m4
-autoreconf --install
+bootstrap () { 
+    rm -rf autom4te.cache install-sh missing Makefile.in configure aclocal.m4
+    autoreconf --install
+}
+
+(cd test/http-get && bootstrap && autoheader)
+bootstrap
--- test/http-get/get.c
+++ test/http-get/get.c
@@ -5,7 +5,7 @@
  */
 
 #if HAVE_CONFIG_H
-# include "config.h"
+# include <config.h>
 #endif
 
 #if STDC_HEADERS
