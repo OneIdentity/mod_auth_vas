OneIdentity/mod_auth_vas

commit 37264f08a28e286bdb61a75bc994e9cae379fc98

Fix some bugs

mod_auth_vas.c
@@ -448,7 +448,7 @@ set_user_obj(request_rec *r)
     if (vaserr) {
 	rn->vas_user_obj = NULL;
 
-	LOG_RERROR(LOG_ERR, 0, r,
+	LOG_RERROR(APLOG_ERR, 0, r,
 		"%s: Failed to get user object for %.100s: %s",
 		__func__,
 		RUSER(r), vas_err_get_string(sc->vas_ctx, 1));
@@ -816,8 +816,13 @@ match_container(request_rec *r, const char *container, int log_level)
     if ((err = rnote_get(sc, r, &rnote)))
 	RETURN(err);
 
-    if ((err = set_user_obj(r)))
-	RETURN(err);
+    if ((vaserr = auth_vas_user_get_vas_user(rnote->user, &vasuser))) {
+	LOG_RERROR(log_level, 0, r,
+		"%s: fatal vas error for user_init: %d, %s",
+		__func__,
+		vaserr, vas_err_get_string(sc->vas_ctx, 1));
+	RETURN(HTTP_FORBIDDEN);
+    }
 
     if ((vaserr = vas_user_get_dn(sc->vas_ctx, sc->vas_serverid, vasuser,
 		    &dn )) != VAS_ERR_SUCCESS ) 
@@ -1233,7 +1238,7 @@ initialize_user(request_rec *request, const char *username) {
 
     /* This is a soft assertion */
     if (rnote->user != NULL) {
-	LOG_RERROR(LOG_ERR, 0, request,
+	LOG_RERROR(APLOG_ERR, 0, request,
 		"%s: User is already set. Overriding it.", __func__);
 	auth_vas_user_unref(rnote->user);
 	rnote->user = NULL;
@@ -1392,6 +1397,7 @@ do_gss_spnego_accept(request_rec *r, const char *auth_line)
 	    goto done;
 	}
 
+	/* FIXME: Call initialize_user instead? */
 	/* Create the remote user object now that we have their name */
 	ASSERT(rn->user == NULL);
 	vaserr = auth_vas_user_alloc(sc->cache, RUSER(r), &rn->user);
@@ -1405,15 +1411,24 @@ do_gss_spnego_accept(request_rec *r, const char *auth_line)
 	}
 
 	/* Save the VAS auth context */
-	vaserr = auth_vas_user_use_gss_result(rn->user, rn->deleg_cred, rn->gss_ctx);
+	{
+	    gss_cred_id_t servercred = GSS_C_NO_CREDENTIAL;
+	    vas_gss_acquire_cred(sc->vas_ctx, sc->vas_serverid, &minor_status, GSS_C_ACCEPT, &servercred);
+
+	vaserr = auth_vas_user_use_gss_result(rn->user, servercred, rn->gss_ctx);
 	if (vaserr) {
 	    result = HTTP_UNAUTHORIZED;
 	    /* We know that the cache & user stuff uses the same vas context as
 	     * the server, but using the vas_ctx here still feels dirty. */
-	    LOG_RERROR(LOG_ERR, 0, r,
+	    LOG_RERROR(APLOG_ERR, 0, r,
 		    "%s: auth_vas_user_use_gss_result failed: %s", __func__,
 		    vas_err_get_string(sc->vas_ctx, 1));
 	    goto done;
+	}
+
+	/* FIXME: free properly */
+	gss_release_cred(&minor_status, &servercred);
+
 	}
 
 	/* Keep a copy of the client's mechanism name in the connection note */
@@ -1432,7 +1447,7 @@ do_gss_spnego_accept(request_rec *r, const char *auth_line)
     } else if (gsserr == GSS_S_CONTINUE_NEEDED) {
 	TRACE_R(r, "waiting for more tokens from client");
 	result = HTTP_UNAUTHORIZED;
-    } else if (memcmp(auth_param, "TlRM", 4) == 0) {
+    } else if (strcmp(auth_param, "TlRM") == 0) {
 	const auth_vas_dir_config *dc = GET_DIR_CONFIG(r->per_dir_config);
 	LOG_RERROR(APLOG_ERR, 0, r,
 		    "NTLM authentication attempted");
@@ -1686,7 +1701,7 @@ auth_vas_server_init(apr_pool_t *p, server_rec *s)
         vas_auth_free(sc->vas_ctx, vasauth);
     }
 
-    sc->cache = auth_vas_cache_new(s);
+    sc->cache = auth_vas_cache_new(s, sc->vas_ctx, sc->vas_serverid);
 }
 
 /**
@@ -1819,7 +1834,7 @@ mav_ip_subnet_cmp(void *rec, const char *key, const char *value)
 	    
 	    mask = slash + 1;
 	    if (!*mask) {
-		LOG_RERROR(LOG_WARNING, 0, r,
+		LOG_RERROR(APLOG_WARNING, 0, r,
 			"Coercing empty netmask to the default (%s)",
 			default_mask);
 		mask = default_mask;
@@ -2268,7 +2283,7 @@ set_remote_user_attr(request_rec *r, const char *attr)
     ASSERT(attr[0]);
 
     if (strcasecmp(attr, "userPrincipalName") == 0) {
-	LOG_RERROR(LOG_DEBUG, 0, r,
+	LOG_RERROR(APLOG_DEBUG, 0, r,
 		"%s: Returning early because REMOTE_USER is already the UPN",
 		__func__);
 	return;
@@ -2278,7 +2293,7 @@ set_remote_user_attr(request_rec *r, const char *attr)
      * per-connection not per-request */
     /* XXX: It might have to be changed to a different attr */
     if(strchr(RUSER(r), '@') == NULL) {
-	LOG_RERROR(LOG_DEBUG, 0, r,
+	LOG_RERROR(APLOG_DEBUG, 0, r,
 		"%s: REMOTE_USER appears to already have been set to %s",
 		__func__, RUSER(r));
 	return;
@@ -2306,7 +2321,7 @@ set_remote_user_attr(request_rec *r, const char *attr)
 	    if (strcasecmp(map->ldap_attr, attr) == 0) {
 		char *attrval;
 
-		LOG_RERROR(LOG_DEBUG, 0, r,
+		LOG_RERROR(APLOG_DEBUG, 0, r,
 			"%s: Using vas cache for lookup of %s attribute",
 			__func__, attr);
 
@@ -2330,7 +2345,7 @@ set_remote_user_attr(request_rec *r, const char *attr)
 	    const char ug = *attr; /* u or g */
 	    struct passwd *pw;
 
-	    LOG_RERROR(LOG_DEBUG, 0, r,
+	    LOG_RERROR(APLOG_DEBUG, 0, r,
 		    "%s: Using vas cache for lookup of %cidNumber attribute",
 		    __func__, ug);
 	    if (vas_user_get_pwinfo(sc->vas_ctx, sc->vas_serverid,
@@ -2348,7 +2363,7 @@ set_remote_user_attr(request_rec *r, const char *attr)
 	}
     }
 
-    LOG_RERROR(LOG_DEBUG, 0, r,
+    LOG_RERROR(APLOG_DEBUG, 0, r,
 	    "%s: VAS cache lookup unavailable for %s, doing LDAP query",
 	    __func__, attr);
 
@@ -2411,7 +2426,7 @@ localize_remote_user(request_rec *r, const char *unused)
 	/* User is probably not Unix-enabled. Try stripping the realm anyway
 	 * for consistency */
 
-	LOG_RERROR(LOG_DEBUG, aprst, r,
+	LOG_RERROR(APLOG_DEBUG, aprst, r,
 		"apr_uid_get failed for %s (normal for non-Unix users), "
 		"using strcmp method",
 		RUSER(r));
@@ -2422,7 +2437,7 @@ localize_remote_user(request_rec *r, const char *unused)
 
     /* Unix-enabled user, convert back to their name */
     if ((aprst = apr_uid_name_get(&username, uid, RUSER_POOL(r))) != OK) {
-	LOG_RERROR(LOG_ERR, aprst, r, "apr_uid_name_get failed for uid %d", uid);
+	LOG_RERROR(APLOG_ERR, aprst, r, "apr_uid_name_get failed for uid %d", uid);
 	return;
     }
 
@@ -2719,7 +2734,7 @@ auth_vas_merge_dir_config(apr_pool_t *p, void *base_conf, void *new_conf)
     return (void *)merged_dc;
 }
 
-/** Passed a auth_vas_server_config pointer */
+/** Passed an auth_vas_server_config pointer */
 static CLEANUP_RET_TYPE
 auth_vas_server_config_destroy(void *data)
 {
@@ -2727,7 +2742,10 @@ auth_vas_server_config_destroy(void *data)
     
     if (sc != NULL) {
 
-	auth_vas_cache_cleanup(sc->cache);
+	if (sc->cache) {
+	    auth_vas_cache_cleanup(sc->cache);
+	    sc->cache = NULL;
+	}
         
 	/* sc->default_realm is always handled by apache */
 

cache.c
@@ -1,7 +1,7 @@
 /*
  * mod_auth_vas: VAS authentication module for Apache.
  *
- *   Copyright 2007 Quest Software, Inc.
+ *   Copyright 2008 Quest Software, Inc.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -43,8 +43,7 @@
  *   (service-to-user) association and should fit most authentication use cases.
  *   In particular it matches the mod_auth_vas use case ;)
  *
- *   The API is vaguely similar to the libvas API so it should be easy to adapt
- *   to, the major differences are:
+ *   Major differences from the libvas API are:
  *   - Users are represented by an auth_vas_user rather than a vas_id_t.
  *   - Credential establishment and authentication are done in one step, by
  *     auth_vas_user_authenticate.
@@ -54,6 +53,8 @@
  *   Most functions are not thread safe. Be careful.
  *
  */
+
+#include <apr_hash.h>
 
 #include "cache.h"
 
@@ -111,11 +112,10 @@ struct auth_vas_cache {
  * own memory pool, but its parent process does. We use that one.)
  */
 auth_vas_cache *
-auth_vas_cache_new(server_rec *server)
+auth_vas_cache_new(server_rec *server, vas_ctx_t *vas_ctx, vas_id_t *vas_serverid)
 {
-    apr_status_t *aprerr;
+    apr_status_t aprerr;
     auth_vas_cache *cache = NULL;
-    auth_vas_server_config *serverconf;
 
     /* Should we allocate the cache out of the cache subpool instead?
      * Only necessary if caches are destroyed more frequently than processes
@@ -143,9 +143,8 @@ auth_vas_cache_new(server_rec *server)
 
     cache->users = apr_hash_make(cache->pool);
 
-    serverconf = GET_SERVER_CONFIG(request->server->module_config);
-    cache->vas_ctx = serverconf->vas_ctx;
-    cache->vas_serverid = serverconf->vas_serverid;
+    cache->vas_ctx = vas_ctx;
+    cache->vas_serverid = vas_serverid;
 
     return cache;
 }
@@ -168,9 +167,11 @@ auth_vas_cache_cleanup(auth_vas_cache *cache)
 	    index;
 	    index = apr_hash_next(index))
     {
+	void *vuserp;
 	auth_vas_user *user;
 
-	user = apr_hash_this(index, NULL, NULL, &user);
+	apr_hash_this(index, NULL, NULL, &vuserp);
+	user = (auth_vas_user*)vuserp;
 	/* No need to remove the user from the hash table, it will be done by
 	 * the pool destructor below. */
 	auth_vas_user_unref(user);
@@ -191,9 +192,9 @@ void
 auth_vas_cache_lock(auth_vas_cache *cache) {
     apr_status_t aprerr;
     
-    aprerr = apr_thread_mutex_lock(&cache->mutex);
+    aprerr = apr_thread_mutex_lock(cache->mutex);
     if (aprerr)
-	LOG_P_ERROR(LOG_CRIT, aprerr, "Cannot lock cache mutex");
+	LOG_P_ERROR(APLOG_CRIT, aprerr, cache->pool, "Cannot lock cache mutex");
 }
 
 /**
@@ -207,9 +208,9 @@ void
 auth_vas_cache_unlock(auth_vas_cache *cache) {
     apr_status_t aprerr;
 
-    aprerr = apr_thread_mutex_unlock(&cache->mutex);
+    aprerr = apr_thread_mutex_unlock(cache->mutex);
     if (aprerr)
-	LOG_P_ERROR(LOG_ERR, aprerr, "Cannot unlock cache mutex");
+	LOG_P_ERROR(APLOG_ERR, aprerr, cache->pool, "Cannot unlock cache mutex");
 }
 
 /**
@@ -252,13 +253,15 @@ vas_err_t
 auth_vas_user_alloc(
 	auth_vas_cache *cache,
 	const char *username,
-	auth_vas_user **user)
+	auth_vas_user **outuser)
 {
-    vas_err_t result; /**< This function's return code. */
-    vas_err_t vaserr; /**< Temp storage */
-    apr_status_t aprerr;
+    vas_err_t result; /* This function's return code. */
+    vas_err_t vaserr; /* Temp storage */
     vas_id_t *local_id;
     auth_vas_user *cached_user;
+
+    LOG_P_ERROR(APLOG_DEBUG, 0, cache->pool,
+	    "%s: Hello World!", __func__);
 
     auth_vas_cache_lock(cache);
     /* Use the RETURN() macro from here on */
@@ -283,19 +286,19 @@ auth_vas_user_alloc(
 	    RETURN(vaserr);
 	}
 
-	cached_user = malloc(sizeof(*cached_user));
+	cached_user = calloc(1, sizeof(*cached_user));
 	cached_user->cache = cache;
 	cached_user->username = strdup(username);
 	cached_user->vas_id = local_id;
 	cached_user->principal_name = upn;
 
 	auth_vas_user_ref(cached_user); /* For the cache */
-	apr_hash_set(cache->users, user->username, APR_HASH_KEY_STRING, user);
+	apr_hash_set(cache->users, cached_user->username, APR_HASH_KEY_STRING, cached_user);
     }
 
     /* Success */
     auth_vas_user_ref(cached_user); /* For the caller */
-    *user = cached_user;
+    *outuser = cached_user;
 
     RETURN(VAS_ERR_SUCCESS);
 
@@ -344,7 +347,7 @@ auth_vas_user_get_vas_user(const auth_vas_user *avuser, vas_user_t **vasuserp)
     /* Use username instead of principal_name because any other name is
      * susceptible to failure when username-attr-name is not userPrincipalName.
      */
-    return vas_user_init(avuser->cache->vas_ctx, avuser->vas_id,
+    return vas_user_init(avuser->cache->vas_ctx, avuser->cache->vas_serverid,
 	    avuser->username, 0, vasuserp);
 }
 
@@ -442,8 +445,8 @@ auth_vas_user_authenticate(
 	int credflags,
 	const char *password)
 {
-    vas_err_t result; /**< Our return code */
-    vas_err_t vaserr; /**< Temp storage */
+    vas_err_t result; /* Our return code */
+    vas_err_t vaserr; /* Temp storage */
 
     if (credflags == user->credflags && user->password != NULL) {
 	/* We already know the user's password */

cache.h
@@ -3,7 +3,7 @@
 /*
  * mod_auth_vas: VAS authentication module for Apache.
  *
- *   Copyright 2007 Quest Software, Inc.
+ *   Copyright 2008 Quest Software, Inc.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -40,6 +40,7 @@
 #include <httpd.h>
 #include <http_log.h>
 #include <vas.h>
+#include <vas_gss.h>
 
 #include "compat.h"
 
@@ -54,7 +55,7 @@ typedef struct auth_vas_user auth_vas_user;
 /* functions */
 
 auth_vas_cache *
-auth_vas_cache_new(server_rec *request);
+auth_vas_cache_new(server_rec *server, vas_ctx_t *vas_ctx, vas_id_t *vas_serverid);
 
 void
 auth_vas_cache_cleanup(auth_vas_cache *cache);
@@ -67,7 +68,7 @@ auth_vas_cache_unlock(auth_vas_cache *cache);
 
 
 vas_err_t
-auth_vas_user_alloc(auth_vas_cache *cache, const char *username, auth_vas_user **user);
+auth_vas_user_alloc(auth_vas_cache *cache, const char *username, auth_vas_user **outuser);
 
 void
 auth_vas_user_ref(auth_vas_user *user);

commit c2f791dca94ee8519b870cbdbca875a8da1b3e49

AuthVasNTLMErrorDocument: add test, fix dir merging, make it work again
There was a bug introduced in 37264f08a28e286bdb61a75bc994e9cae379fc98 that
broke detection of NTLM requests. It is not in any released version.

mod_auth_vas.c
@@ -1572,7 +1572,7 @@ do_gss_spnego_accept(request_rec *r, const char *auth_line)
     } else if (gsserr == GSS_S_CONTINUE_NEEDED) {
 	TRACE_R(r, "waiting for more tokens from client");
 	result = HTTP_UNAUTHORIZED;
-    } else if (strcmp(auth_param, "TlRM") == 0) {
+    } else if (strncmp(auth_param, "TlRM", 4) == 0) {
 	const auth_vas_dir_config *dc = GET_DIR_CONFIG(r->per_dir_config);
 	LOG_RERROR(APLOG_ERR, 0, r,
 		    "NTLM authentication attempted");
@@ -2993,6 +2993,10 @@ auth_vas_merge_dir_config(apr_pool_t *p, void *base_conf, void *new_conf)
 	    else
 		merged_dc->ntlm_error_document = apr_pstrdup(p,
 			new_dc->ntlm_error_document);
+	} else if (base_dc->ntlm_error_document) {
+	    /* Inherit. strdup is probably unnecessary */
+	    merged_dc->ntlm_error_document = apr_pstrdup(p,
+		    base_dc->ntlm_error_document);
 	}
     }
     return (void *)merged_dc;

test/Makefile.am
@@ -29,6 +29,7 @@ RCDEV_TESTS = \
 	test.bug369 \
 	test.bug482 \
 	test.bug495 \
+	test.bug210 \
 	# end of RCDEV tests
 
 EXTRA_DIST= get-platform run-test $(RCDEV_TESTS)

test/test.bug210
@@ -1,0 +1,85 @@
+#!/bin/sh ./run-test
+# bug 210: Have mod_auth_vas return error text on NTLM attempts
+description "NTLM-specific error page"
+
+# We can't test
+#   AuthVasNTLMErrorDocument /page
+# because it is relative to the DocumentRoot but we don't know what the
+# relationship between $DOC_DIR and DocumentRoot are.
+
+TESTFILE=test.$$
+OUTPUT=/tmp/output.$$
+
+cleanup () {
+   ap_stop
+   ap_conf_restore
+   rm -f $DOC_DIR/$TESTFILE $OUTPUT $DOC_DIR/subdir/$TESTFILE $DOC_DIR/errdoc
+   rmdir $DOC_DIR/subdir
+}
+trap cleanup 0 1 2
+
+ap_conf_save
+
+ap_conf_append <<-.
+	LogLevel debug
+	<Directory "$DOC_DIR">
+		AuthType VAS
+		Require valid-user
+	</Directory>
+.
+ap_conf_check
+
+echo "$$ hello" > $DOC_DIR/$TESTFILE
+echo "This is the error document with keyword: babelshark" > $DOC_DIR/errdoc
+mkdir $DOC_DIR/subdir
+echo "$$ hello" > $DOC_DIR/subdir/$TESTFILE
+
+ap_start
+
+fetch_fake_ntlm `ifdebug -d` $DOC_URL/$TESTFILE > $OUTPUT &&
+	fail "NTLM was not meant to succeed (using default ntlm errordoc [default])"
+$NDEBUG || cat $OUTPUT
+grep NTLM $OUTPUT >/dev/null || fail "ErrorDocument did not mention NTLM"
+
+ap_stop
+ap_conf_restore
+
+ap_conf_append <<-.
+	LogLevel debug
+	<Directory "$DOC_DIR">
+		AuthType VAS
+		Require valid-user
+		AuthVasNTLMErrorDocument "fizzle $$"
+	</Directory>
+.
+ap_conf_check
+ap_start
+
+fetch_fake_ntlm `ifdebug -d` $DOC_URL/$TESTFILE > $OUTPUT &&
+    fail "NTLM was not meant to succeed (using string ntlmerrordoc [fizzle])"
+$NDEBUG || cat $OUTPUT
+grep fizzle $OUTPUT > /dev/null || fail "ErrorDocument did not mention fizzle"
+
+ap_stop
+ap_conf_restore
+ap_conf_append <<-.
+	LogLevel debug
+	<Directory "$DOC_DIR">
+		AuthType VAS
+		Require valid-user
+		AuthVasNTLMErrorDocument $DOC_URI/errdoc
+	</Directory>
+	<Files "$DOC_DIR/errdoc">
+		Satisfy any
+		Order allow,deny
+		Allow from all
+	</Files>
+.
+
+ap_conf_check
+ap_start
+
+fetch_fake_ntlm `ifdebug -d` $DOC_URL/$TESTFILE > $OUTPUT &&
+    fail "NTLM was not meant to succeed (using file ntlmerrordoc [babelshark])"
+$NDEBUG || cat $OUTPUT
+grep babelshark $OUTPUT > /dev/null || fail "ErrorDocument did not mention babelshark"