6529850bf4b7b54979958f07ac0831ccff64eb60
Ted Percival <ted.percival@quest.com>
2008-07-07 16:21:15 +1000

    Overwrite libtool files during bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,11 @@
 #! /bin/sh
-# (c) 2006 Quest Software, Inc. All rights reserved.
+# (c) 2008 Quest Software, Inc. All rights reserved.
 # Initialises the directory with autoconf after a respoitory check-out.
 
 bootstrap () { 
     (set -x; cd "$1"
      rm -rf autom4te.cache install-sh missing Makefile.in configure aclocal.m4 config.h.in
-     autoreconf --install
+     autoreconf --force --install
     )
 }
 
